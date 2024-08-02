1c7e02a69253f0327749851fc122277b9d675585
Lakshminp <badri.dilbert@gmail.com>
2024-08-02 09:47:04 +0530

    Add logs

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 import os
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app. Edited at 8:48 PM, 14th July 2024."})
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    app.logger.info(f'Request received at {timestamp}')
+    return jsonify({"Choo Choo": "Welcome to your Flask app. Edited at 9:46 PM, 2nd August 2024."})
 
 
 if __name__ == '__main__':
