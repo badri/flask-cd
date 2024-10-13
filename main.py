22dbea593aef92abcce601576fab66c792149251
Lakshminp <badri.dilbert@gmail.com>
2024-10-13 18:50:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 def index():
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     logging.info(f'Request received at {timestamp}')
-    return jsonify({"Choo Choo": "Welcome to your Flask app. Edited at 6:18 PM, 13th Oct 2024."})
+    return jsonify({"Choo Choo": "Welcome to your Flask app. Edited at 6:50 PM, 13th Oct 2024."})
 
 
 if __name__ == '__main__':
